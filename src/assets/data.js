f936499e0b8a8655b9fdcbe120eae7d7e0654800
Siyi <syan0155@student.monash.edu>
2025-08-29 18:27:29 +1000

    data

---
+++ src/assets/data.js
@@ -0,0 +1,5 @@
+export const classes = [
+  { id: 1, title: "Strength Training", level: "Beginner", duration: 45 },
+  { id: 2, title: "Cardio Blast", level: "Intermediate", duration: 30 },
+  { id: 3, title: "Yoga Flow", level: "All", duration: 60 },
+];
