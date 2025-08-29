bb24dfd52e94bd6a1da41958dfd54511b62287ec
Siyi <syan0155@student.monash.edu>
2025-08-29 18:19:22 +1000

    update NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark navbar-expand">
+    <div class="container">
+      <RouterLink to="/" class="navbar-brand">My Vue App</RouterLink>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><RouterLink to="/" class="nav-link">Home</RouterLink></li>
+        <li class="nav-item"><RouterLink to="/form" class="nav-link">Form</RouterLink></li>
+        <li class="nav-item"><RouterLink to="/about" class="nav-link">About</RouterLink></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
